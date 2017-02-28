bd2bc0cfd0ab5688e1cedaac773f3ef4f4d80510
CatLoverKid <aclkeba@gmail.com>
2017-02-28 12:40:33 -0800

    added for looping

--- SumoSolver/MyTuple.java
+++ SumoSolver/MyTuple.java
@@ -1,24 +1,8 @@
 class MyTuple {
 	private int[] values = new int[2];
-	
-	public MyTuple(int cost, int gain) {
-		values[0] = cost;
-		values[1] = gain;
-	}
-	
-	public int getCost() {
-		return values[0];
-	}
-	
-	public int getGain() {
-		return values[1];
-	}
-	
-	public void setCost(int Cost) {
-		values[0] = Cost;
-	}
-	
-	public void setGain(int Gain) {
-		values[1] = Gain;
-	}
+	public MyTuple(int cost, int gain) {values[0] = cost;values[1] = gain;}
+	public int getCost() {return values[0];}
+	public int getGain() {return values[1];}
+	public void setCost(int Cost) {values[0] = Cost;}
+	public void setGain(int Gain) {values[1] = Gain;}
 }
--- SumoSolver/SumoSolver.java
+++ SumoSolver/SumoSolver.java
@@ -1,46 +1,46 @@
 import java.io.*;
 
-class SumoSolver {
-  static int amountToSpend = 0;
- 
- 
- 
-  int partition (MyTuple array[], int left, int right){
-	  int i = left;
-	  int j = right;
-	  MyTuple temp;
-	  MyTuple pivot = array[(left + right) / 2];
-	  
-	  while(i <= j){
-		  while(array[i].getCost() < pivot.getCost()){i++;}
-		  while(array[i].getCost() > pivot.getCost()){j --;}
-		  if(i <= j){
-			  temp = array[i];
-			  array[i] = array [j];
-			  array[j] = temp;
-			  i++;
-			  j--;
-		  }
-		  
-	  }
-	  return i;
-  }
-  
-  MyTuple quickSort(MyTuple array[], int left, int right){
-	int index = partition(array, left, right);
-	if(left < index - 1){quickSort(array, left, right);}
-	if(index < right){quickSort(array, index, right);}
-  }
- 
-
- 
-  public static void main(String args[]){
-	MyTuple[] products = new MyTuple[(args.length - 1) / 2];
-    amountToSpend = Integer.parseInt(args[args.length - 1]);
-
-    for(int i = 0; i < args.length - 1; i+= 2){
-      products[i / 2] = new MyTuple(Integer.parseInt(args[i]), Integer.parseInt(args[i + 1]));
+    class Food {
+        int weight ;
+        int cost;
+        boolean used;
+        public Food (int Weight, int Cost, boolean Used){
+            weight = Weight;
+            cost = Cost;
+            used = Used;
+        }
+        public String toString(){
+            return "(Weight: " + weight + ", Cost: $" + cost + ") ";
+        };
     }
-	products = quickSort(products, 0, products.length - 1);
- }
+    
+class SumoSolver {
+    public static void storeToString () {
+    System.out.println("Money to Spend: $" + amountToSpend);
+    for(int i = 0; i < store.length; i ++)if(store[i].used){System.out.print(store[i].toString());}
+    }
+    
+    static int amountToSpend = 0;
+    static Food[] store;
+    static Food[][][] memo;
+    static Food[][] table;
+ 
+    public static void main(String args[]){
+        store = new Food[(args.length - 1) / 2];
+        amountToSpend = Integer.parseInt(args[args.length - 1]);
+        for(int i = 0; i < args.length - 1; i+= 2){store[i / 2] = new Food(Integer.parseInt(args[i]), Integer.parseInt(args[i + 1]), false);}
+        table = new Food[amountToSpend][store.length];
+        memo = new Food[amountToSpend][store.length][amountToSpend*store.length];
+        
+        for(int i = 0; i < amountToSpend; i++){
+            for(int j = 0; j < store.length; j ++){
+                if(store[j] > i){
+                    store[j].used = true;
+                    
+                }
+            }
+        }
+    }
 }
+
+
