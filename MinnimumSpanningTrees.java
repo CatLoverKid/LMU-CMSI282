d0724261fcaa9833a6e5defc6bc733d22a98b4bc
CatLoverKid <aclkeba@gmail.com>
2017-03-14 12:45:55 -0700

    added Minnimum Spanning Tree file

---
+++ MinnimumSpanningTrees.java
@@ -0,0 +1,12 @@
+//	INPUT: Undirected, Weighted, Graph
+//	OUTPUT: A FREE Tree that is a subgraph of the original, which uses all the nodes and whose composite weight is minimal
+
+//      GREEDY OPTION: Prim's Algorithm:
+//		Start at a random point, add the closest point, continue to pick now with the new node's neighbors included in the list of options
+
+// 	GREEDY OPTION: Kruskal's Algorithm
+//		At Each Iteration, coalesce two disjoint structures via cheapest edge
+//			This method requires the edges to be pre-sorted by weight (cost)
+class MinnimumSpanningTrees {
+
+}
